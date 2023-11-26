69fd1a07cb79465d419ee60a59325ab87f2d7773
datta88 <dattatupe77@gmail.com>
2023-11-26 14:33:34 +0530

    added normal react app

---
+++ src/view/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+        <h1>Welcome !</h1>
+        <p>we hope you enjoy learning React. </p>
+        <h3>Have greate day !</h3>
+        </>
+    )
+}
+export default Home
